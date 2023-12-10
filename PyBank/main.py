ad3432d959d58a4a657ce925233b54499e173fb5
amandaedunaway <amandaedunaway@gmail.com>
2023-12-09 20:21:55 -0600

    Added comments to python file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,46 +1,48 @@
+# Import library and establish file paths to read and write
 import csv
 
 file_path = r'C:\Users\aed3h\Documents\Data_Analysis\Module 3 Challenge\python-challenge\PyBank\Resources\budget_data.csv'
 output_path = r'C:\Users\aed3h\Documents\Data_Analysis\Module 3 Challenge\python-challenge\PyBank\Analysis\budget_analysis.txt'
 
-total_months = 0
-total_net = 0
-#month_of_change = []
-net_change_list = []
-greatest_inc = [0,""]
-greatest_dec = [999999999,""]
-# print("Financial Analysis")
-# print("----------------------------")
+
+# Initialize variables and lists
+total_months = 0                # total months in the dataset
+total_net = 0                   # net total amount of "Profit/Losses"
+net_change_list = []            # empty list to fill in with month-to-month changes
+greatest_inc = [0,""]           # list to fill in greatest increase in profits and corresponding date
+greatest_dec = [999999999,""]   # list to fill in greatest decrease in profits and corresponding date
 
 
-##Find Total number of months:
+# Open the file and find results
 with open(file_path, 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    header = next(csv_reader)
-    first_line = next(csv_reader)
-    total_months += 1
-    total_net += int(first_line[1])
-    previous = int(first_line[1])
-    for row in csv_reader:
-        total_months +=1
-        total_net += int(row[1])
-        net_change = int(row[1]) - previous
-        net_change_list.append(net_change)
-        previous = int(row[1])
-        #month_of_change.append(row[0])
+    csv_reader = csv.reader(csv_file)       # read the file
+    header = next(csv_reader)               # designate header row as first row of file
+    first_line = next(csv_reader)           # designate the first row of data as second row of file
+    total_months += 1                       # add 1 to the count of months
+    total_net += int(first_line[1])         # add the profit for the row to the cumulative total 
+    previous = int(first_line[1])           # designate the profit as the "previous" value for future comparison
 
-        if net_change > greatest_inc[0]:
-            greatest_inc[0] = net_change
-            greatest_inc[1] = row[0]
+    for row in csv_reader:                  # For each remaining row in the data, do the following:
+        total_months +=1                        # add 1 to the count of months
+        total_net += int(row[1])                # add the profit for the row to the cumulative total
+        net_change = int(row[1]) - previous     # subtract last month's profit from this month's profit
+        net_change_list.append(net_change)      # add this month's change in profit to the net change list
+        previous = int(row[1])                  # designate this month's profit as the "previous" value for next row
 
-        if net_change < greatest_dec[0]:
-            greatest_dec[0] = net_change
-            greatest_dec[1] = row[0]
+        if net_change > greatest_inc[0]:        # If this month's change in profit exceeds the greatest increase in profit so far,
+            greatest_inc[0] = net_change        # record this month's change in profit as the greatest increase
+            greatest_inc[1] = row[0]            # and record this date 
 
-         # Total number of months in the dataset = number of rows
-        #row_count = sum(1 for row in csv_reader) + 1
-#print("Total Months: "+str(row_count))
+        if net_change < greatest_dec[0]:        # If this month's change in profit is lower than the greatest decrease in profit so far,
+            greatest_dec[0] = net_change        # record this month's change in profit as the greatest decrease
+            greatest_dec[1] = row[0]            # and record this date
+
+# Calculate the average change of the dataset by summing all the monthly changes and dividing by the number of data points in the list
 monthly_average_change = sum(net_change_list)/len(net_change_list)
+
+
+# Set up the text outline for the analysis results and input the values found above
+# Display the results in the terminal
 results = f"""
 Financial Analysis
 ----------------------------
@@ -51,10 +53,9 @@ Greatest Increase in Profits: {greatest_inc[1]} (${greatest_inc[0]})
 Greatest Decrease in Profits: {greatest_dec[1]} (${greatest_dec[0]})
 """
 print(results)
-# ##Find Net total amount of Profit/Losses:
-# column_to_sum = 'Profit/Losses'
-# column_sum = 0
 
+
+# Write the results to a txt file, located in the specified output path
 with open(output_path, 'w') as txt_file:
     txt_file.write(results)
 
