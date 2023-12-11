f0c1f8db71e1d5baae4b3e7942e9b73f4ddd37fb
amandaedunaway <amandaedunaway@gmail.com>
2023-12-11 12:22:26 -0600

    Final version of script with txt and readme

--- PyPoll/README.md
+++ PyPoll/README.md
@@ -2,8 +2,12 @@
 
 Module 3 Challenge: PyPoll
 
-The “main.py” Python script 
+The “main.py” Python script analyzes voters' choices in an election. The script reads the .csv file and scans the data to return the candidate names and the election results. Election results include the total number of votes in the election, the count of votes for each candidate, the percentage of votes for each candidate, and the winning candidate.
+
+
+Comments about the code:
+Both Sharon and the BCS "Hints" section mentioned using dictionaries. However, I chose to stick to lists only.
 
 
 Sources:
-
+Beyond class notes for "len()", "index()", "append()", etc., I received help from Sharon, a BCS tutor, on 12/8. We primarily worked on the code for PyBank. At the end of my session, she gave me a few hints on how to proceed with PyPoll. The hints included using a for loop to count the votes and using "not in" when tracking candidate names. Both her guidance with PyBank and her hints for PyPoll made my time programming and the resulting code much more efficient than it would have been otherwise, if I had followed only what I had written for the pseudocode.
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,47 +1,78 @@
-#import necessary libraries
+# Import library and establish file paths to read and write
+import csv
 
-# read the csv file: input with file path 
-# seperate the data with comma delimiter
-# need to be able to read and write to the file?
-
-
-#print("Election Results")
-#print("-------------------------")
-
-# for loop to utilize all rows
-    #for i from 2 to length of data
-
-        # Total number of votes in the dataset:
-            #calculation: sum the number of i's (starting from line 2) 
-            # or we could just say the total votes is the length of data - 1 (no loop needed)
-            # we can define the length of the data as a variable to refer to each time
-                #print("Total Votes:": total votes calc)
-
-    #print("-------------------------")
-
-        # Complete list of candidates who received votes:
-            #calculation: Candidates() = use the "unique" function to find the unique names
-            #creates a list or dictionary of candidates
-
-        # The percentage and total votes each candidate won:
-            # is there a count function? could search column 3 and use "count" for each name
-            # or search all rows and count the number of times each name appears: 
-                # ccc = 0
-                # if row i, column 3 = "Charles Casper Stockham" then ccc = ccc + 1 else nothing.
-                # calculation: (ccc/total votes calc)*100
-                #print("Candidates(0):": total votes calc (ccc))
-            # do the same for other 2 candidates 
-
-    #print("-------------------------")
-
-# The winner of the election based on popular vote
-    #calculation: find the max value of Candidates()
-    #print("Winner:", max value candidates calculation)
-
- #print("-------------------------")
+file_path = r'C:\Users\aed3h\Documents\Data_Analysis\Module 3 Challenge\python-challenge\PyPoll\Resources\election_data.csv'
+output_path = r'C:\Users\aed3h\Documents\Data_Analysis\Module 3 Challenge\python-challenge\PyPoll\Analysis\election_analysis.txt'
 
 
-#Addtional requirements:
-#Analysis prints to terminal as code runs
-#Export text file with the results:
-    #unsure how to do this part
+
+# Initialize variables and lists
+total_votes = 0                             # total votes in the dataset
+candidates_list = []                        # empty list to fill in candidate names
+votes_per_cand = [0,0,0]                    # list to fill in the count of each candidate's votes
+votes_per_cand_perc = [0,0,0]               # list to calculate the each candidates percentage of total votes
+
+
+
+# Open the file and collect the information needed from the file
+with open(file_path, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)       # read the file
+
+    # Set up all initial information needed prior to starting the for loop for the rest of the data
+    header = next(csv_reader)               # designate header row as first row of file
+    first_line = next(csv_reader)           # designate the first row of data as second row of file
+    
+    total_votes += 1                        # add 1 to the count of votes for the first row of data
+
+    # add the name in the first row of data to the candidate list
+    candidates_list.append(first_line[2])
+    # add a vote to the count for the candidate in the corresponding spot in the list   
+    votes_per_cand[candidates_list.index(first_line[2])] += 1
+
+
+    # For each remaining row in the data, do the following:
+    for row in csv_reader:                  
+        total_votes +=1                     # add 1 to the count of total votes
+
+        # Check if the candidate name in this row matches any of the candidate names recorded in the list.
+        # If it is a new name, add it to the candidate list.
+        if row[2] not in candidates_list:
+            candidates_list.append(row[2])
+        # add a vote to the count for the candidate in the corresponding spot in the list
+        votes_per_cand[candidates_list.index(row[2])] += 1
+
+
+
+# Use the information gathered from the file to find the relevant results
+# Calculate the percentage of total votes for each candidate, rounded to three decimal places
+for candidate in range(len(candidates_list)):
+    votes_per_cand_perc[candidate] = round((votes_per_cand[candidate] / total_votes) * 100,3)
+
+
+# Determine the winner
+win_cand_count = max(votes_per_cand)                    # the winning number of votes is the maximum number of votes received by a candidate 
+winner_index = votes_per_cand.index(win_cand_count)     # record which spot in the list the winning number was found
+winner = candidates_list[winner_index]                  # determine the name of the winner by matching to the corresponding spot in the list
+
+
+# Set up the text outline for the analysis results and input the values found above
+# Display the results in the terminal
+results = f"""
+Election Results
+-------------------------
+Total Votes: {total_votes}
+-------------------------
+{candidates_list[0]}: {votes_per_cand_perc[0]}% ({votes_per_cand[0]})
+{candidates_list[1]}: {votes_per_cand_perc[1]}% ({votes_per_cand[1]})
+{candidates_list[2]}: {votes_per_cand_perc[2]}% ({votes_per_cand[2]})
+-------------------------
+Winner: {winner}
+-------------------------
+"""
+print(results)
+
+
+
+# Write the results to a txt file, located in the specified output path
+with open(output_path, 'w') as txt_file:
+    txt_file.write(results)
